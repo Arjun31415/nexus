73fdc9c1599acb9dfe972842eaf5fa5fb4d6d93e
Arjun31415 <arjunp0710@gmail.com>
2023-08-02 14:35:51 +0530

    added rust overlay

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.extraSpecialArgs = {inherit inputs;};
-            home-manager.useGlobalPkgs = true;
+#            home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.prometheus = import ./home;
           }
--- home/dev/default.nix
+++ home/dev/default.nix
@@ -3,13 +3,13 @@
   inputs,
   ...
 }: {
+  nixpkgs.overlays = [inputs.rust-overlay.overlays.default];
   home.packages = with pkgs; [
     gh
     niv
     wakapi
+    rust-bin.nightly.latest.default
   ];
-  /* nixpkgs.overlays = [inputs.rust-overlay.overlays.default];
-  home.packages = [pkgs.rust-bin.nightly.latest.default]; */
   programs.neovim = {
     package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
     vimAlias = true;
