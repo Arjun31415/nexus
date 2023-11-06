2ee4cf506f4d879abe4c1001d2563abbfe006538
Arjun31415 <arjunp0710@gmail.com>
2023-11-06 09:02:32 +0530

    updated flake

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -75,7 +75,6 @@ in {
           enable = true;
           enableOffloadCmd = true;
         };
-        #  sync.enable = true;
         nvidiaBusId = "PCI:1:0:0";
         amdgpuBusId = "PCI:6:0:0";
       };
