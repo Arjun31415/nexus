3669fe1cb77ac13390bced7f84a41f7612cbf319
Arjun31415 <arjunp0710@gmail.com>
2023-11-27 17:36:20 +0530

    working hyprland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -12,7 +12,7 @@ in {
   wayland.windowManager.hyprland = {
     enable = true;
     package = hyprland.packages.${pkgs.system}.hyprland;
-    plugins = [hy3.packages.${pkgs.system}.default hyprland-plugins.packages.${pkgs.system}.hyprwinwrap];
+   # plugins = [hy3.packages.${pkgs.system}.default hyprland-plugins.packages.${pkgs.system}.hyprwinwrap];
     extraConfig = ''
       source = ${impurity.link ./hyprland.conf}
     '';
