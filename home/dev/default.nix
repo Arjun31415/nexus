ab33eb1744e0491341df14535a9b8dbaea7505e2
Arjun31415 <arjunp0710@gmail.com>
2024-02-11 20:46:37 +0530

    added foot config

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -99,7 +99,35 @@ in {
     server.enable = true;
     settings = {
       main = {
+        font = "Fisa Code:style=Regular:size=10";
         dpi-aware = "yes";
+      };
+      colors = {
+        background = "24283b";
+        alpha = 0.85;
+        ## Normal/regular colors (color palette 0-7)
+        regular0 = "1d202f";
+        regular1 = "f7768e";
+        regular2 = "9ece6a";
+        regular3 = "e0af68";
+        regular4 = "7aa2f7";
+        regular5 = "bb9af7";
+        regular6 = "7dcfff";
+        regular7 = "a9b1d6";
+
+        # bright colors (color palette 8-15)
+
+        bright0 = "414868";
+        bright1 = "f7768e";
+        bright2 = "9ece6a";
+        bright3 = "e0af68";
+        bright4 = "7aa2f7";
+        bright5 = "bb9af7";
+        bright6 = "7dcfff";
+        bright7 = "c0caf5";
+        ## The remaining 256-color palette
+        "16" = "ff9e64";
+        "17" = "db4b4b";
       };
       mouse = {
         hide-when-typing = "yes";
