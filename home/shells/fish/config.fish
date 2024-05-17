30f019176d4d99a6af21474bd59df803c9806486
Arjun31415 <arjunp0710@gmail.com>
2024-05-17 10:02:51 +0530

    fastfetch config update

--- home/shells/fish/config.fish
+++ home/shells/fish/config.fish
@@ -44,7 +44,7 @@ end
 
 if type -q fastfetch
     if test -d ~/.config/fastfetch/presets
-        fastfetch --load-config ~/.config/fastfetch/presets/neofetch
+        fastfetch --load-config ~/.config/fastfetch/presets/neofetch.jsonc
     else
         fastfetch
     end
