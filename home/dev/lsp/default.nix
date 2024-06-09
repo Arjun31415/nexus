2b33a894d4e7f219e9b5dc13e08b47739b78efbc
Arjun31415 <arjunp0710@gmail.com>
2024-06-09 10:07:00 +0530

    updated system

--- home/dev/lsp/default.nix
+++ home/dev/lsp/default.nix
@@ -3,23 +3,26 @@
   inputs,
   ...
 }: {
-  home.packages = with pkgs; [
-    stylua
-    lua-language-server
-    inputs.nix-nil-lsp.packages.${pkgs.system}.default
-    clang-tools_17
-    shellcheck
-    nodePackages_latest.bash-language-server
-    nodePackages_latest.pyright
-    nodePackages_latest.typescript-language-server
-    nodePackages_latest.vim-language-server
-    nodePackages.vscode-css-languageserver-bin
-    nodePackages_latest.vscode-json-languageserver-bin
-    prettierd
-    cmake-language-server
-    rust-analyzer
-    # inputs.nix-nixd-lsp.packages.${pkgs.system}.default
-    # Linters
-    selene # lua
-  ];
+  home.packages = with pkgs;
+    [
+      stylua
+      lua-language-server
+      inputs.nix-nil-lsp.packages.${pkgs.system}.default
+      clang-tools_17
+      shellcheck
+      pyright
+      prettierd
+      cmake-language-server
+      rust-analyzer
+      # inputs.nix-nixd-lsp.packages.${pkgs.system}.default
+      # Linters
+      selene # lua
+    ]
+    ++ (with pkgs.nodePackages_latest; [
+      bash-language-server
+      typescript-language-server
+      vim-language-server
+      vscode-css-languageserver-bin
+      vscode-json-languageserver-bin
+    ]);
 }
