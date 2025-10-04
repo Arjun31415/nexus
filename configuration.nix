8a64f5b3a430b661f006be78311e808e58e110be
Arjun31415 <arjunp0710@gmail.com>
2025-10-04 17:19:54 +0200

    added nix community cuda cachix

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,7 @@ in {
       "https://nix-community.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://prismlauncher.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-substituters = [
       "https://hyprland.cachix.org"
@@ -83,6 +84,7 @@ in {
       "https://nix-community.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://prismlauncher.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-public-keys = [
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
@@ -94,6 +96,7 @@ in {
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 
