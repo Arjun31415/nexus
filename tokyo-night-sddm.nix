efdc427caef1e970ea6eb00ab7cfc1d53ade1879
Arjun31415 <arjunp0710@gmail.com>
2025-11-19 12:58:59 +0100

    update for qt6

--- tokyo-night-sddm.nix
+++ tokyo-night-sddm.nix
@@ -10,10 +10,10 @@ stdenvNoCC.mkDerivation
   name = "tokyo-night-sddm";
   dontBuild = true;
   src = fetchFromGitHub {
-    owner = "xaknick";
+    owner = "Arjun31415";
     repo = "tokyo-night-sddm-qt6";
-    rev = "ffc26208bb6ddd033d1fe945d19b60e4e1b002b2";
-    sha256 = "sha256-Tk0hXKFT/uE1ncIHSEwIC26Z/wC4wXb/7CnY3lBGzFM=";
+    rev = "9862a74ed77c840b1d3e5f7f86a814afdf367884";
+    sha256 = "sha256-r6k7QUpCG2GAp28nUphymVrLm08Re9eqmc/ErynFCDI=";
   };
   patchPhase = ''
     sed -i 's|Background="Backgrounds/win11.png"|Background="Backgrounds/tokyocity.png"|g' theme.conf
@@ -33,7 +33,7 @@ stdenvNoCC.mkDerivation
   installPhase = ''
     set -x
     mkdir -p $out/share/sddm/themes
-    # $src is the unmodifed read only source originally cloned.
+    # $src is the unmodified read only source originally cloned.
     cp -r . $out/share/sddm/themes/tokyo-night-sddm
     set +x
   '';
